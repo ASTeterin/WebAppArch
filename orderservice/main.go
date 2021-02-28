68b4efa72336b11cb306d27dac91e22f664842a0
ASTeterin <at28813905@gmail.com>
2021-02-28 22:27:52 +0300

    отдаю JSON

--- orderservice/main.go
+++ orderservice/main.go
@@ -1,27 +1,57 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 	"orderservice/transport"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
+func getKillSignalChan() chan os.Signal {
+	osKillSignalchan := make (chan os.Signal, 1)
+	signal.Notify(osKillSignalchan, os.Interrupt, syscall.SIGTERM)
+	return osKillSignalchan
+}
 
+func waitForKillSignall(killSignalChan <-chan  os.Signal) {
+	killSignal := <-killSignalChan
+	switch killSignal {
+	case os.Interrupt:
+		log.Info("got SIGINT...")
+	case syscall.SIGTERM:
+		log.Info("got SIGTERM")
+	}
+}
+
+func startServer(serverURL string) *http.Server {
+	router := transport.Router()
+	srv := &http.Server{Addr: serverURL, Handler: router}
+	go func() {
+		log.Fatal(srv.ListenAndServe())
+	}()
+	return srv
+}
 
 func HelloHandler(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprint(w, "hello")
 }
 
 func main() {
-	router := transport.Router()
-	fmt.Println(http.ListenAndServe(":8000", router))
-	/*http.HandleFunc("/hello-world", HelloHandler)
-	http.HandleFunc("/api/v1/orders", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprint(w, orders)
-	})
-	http.HandleFunc("/api/v1/order/d290f1ee-6c54-4b01-90E6-d701748fo851", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprint(w, order)
-	})
-	//http.HandlerFunc("/hello-world", HelloHandler)
-	http.ListenAndServe(":8000", nil)*/
+	log.SetFormatter(&log.JSONFormatter{})
+	file, err := os.OpenFile("my.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err == nil {
+		log.SetOutput(file)
+		defer file.Close()
+	}
+	serverUrl := ":8000"
+	log.WithFields(log.Fields{"url": serverUrl}).Info("starting server")
+	killSignalChan := getKillSignalChan()
+	srv := startServer(serverUrl)
+
+	waitForKillSignall(killSignalChan)
+	srv.Shutdown(context.Background())
 }
--- orderservice/transport/handlers.go
+++ orderservice/transport/handlers.go
@@ -1,9 +1,15 @@
 package transport
 
 import (
+	"encoding/json"
+	//"encoding/json"
+	//"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+	"io"
 	"net/http"
+	"time"
 )
 
 var orders = `[{
@@ -24,13 +30,55 @@ var order = `[{
 	"cost": 999
 }]`
 
-func Router() *mux.Router {
+type Menu struct {
+	Id       string `json:"id"`
+	Quantity int    `json:"quantity"`
+}
+
+type Order struct {
+	Id        string `json:"id"`
+	Menuitems Menu`json:"menuitems"`
+}
+
+
+func getOrder(w http.ResponseWriter, _ *http.Request) {
+	var menuitem = Menu{
+		Id: "f290d1ce-6c234-4b31-90e6-d701748fo851",
+		Quantity: 1,
+	}
+	var order = Order{
+		Id:        "d290f1ee-6c54-4b01-90E6-d701748fo851",
+		Menuitems: menuitem,
+	}
+	b, _ := json.Marshal(order)
+	w.Header().Set("Content-type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, string(b))
+}
+
+func Router() http.Handler {
 	r := mux.NewRouter()
 	s := r.PathPrefix("/api/v1").Subrouter()
 	s.HandleFunc("/hello-world", helloWorld).Methods(http.MethodGet)
-	s.HandleFunc("/orders", getOrders)
+	s.HandleFunc("/orders", getOrder)
+	return logMiddleware(r)
+}
 
-	return r
+func logMiddleware(h http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now()
+		endTime := time.Now()
+		log.WithFields(log.Fields{
+			"method": r.Method,
+			"url": r.URL,
+			"remoteAddr": r.RemoteAddr,
+			"time": int(endTime.Sub(startTime)),
+		}).Info("got a new request")
+		h.ServeHTTP(w, r)
+		endTime = time.Now()
+	})
+
 }
 
 func getOrders(w http.ResponseWriter, _ *http.Request) {
