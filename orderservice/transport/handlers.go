6c029c54f100055288d1c109588d6997dd631279
ASTeterin <at28813905@gmail.com>
2021-03-07 21:55:46 +0300

    изменил структуры в соответствии с API

--- orderservice/transport/handlers.go
+++ orderservice/transport/handlers.go
@@ -3,6 +3,7 @@ package transport
 import (
 	"encoding/json"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 	"io"
@@ -10,40 +11,52 @@ import (
 	"time"
 )
 
-type Menu struct {
+type order struct {
+	Id    string `json:"id"`
+	Items []menuItem
+}
+
+type orderResponse struct {
+	order
+	OrderedAtTimeStamp string `json:"orderedAtTimeStamp"`
+	Cost               int    `json:"cost"`
+}
+
+type menuItem struct {
 	Id       string `json:"id"`
 	Quantity int    `json:"quantity"`
 }
 
-type Order struct {
-	Id        string `json:"id"`
-	Menuitems Menu`json:"menuitems"`
-	OrderedAtTimestamp int `json:"orderedAtTimestamp"`
-	Cost int `json:"cost"`
+type orders struct {
+	Orders []order
 }
 
-type Orders struct {
-	Id        string `json:"id"`
-	Menuitems Menu`json:"menuitems"`
-}
-
-var menuitem = Menu{
+var menuitem = menuItem{
 	Id:       "f290d1ce-6c234-4b31-90e6-d701748fo851",
 	Quantity: 1,
 }
-var order = Order {
-	Id:        "d290f1ee-6c54-4b01-90E6-d701748fo851",
-	Menuitems: menuitem,
-	OrderedAtTimestamp: 1613758423,
-	Cost: 999,
+
+var newOrder = order{
+	Id:  "d290f1ee-6c54-4b01-90E6-d701748fo851",
+	Items: []menuItem{
+		menuitem,
+	},
 }
 
-var orders = Orders{
-	Id:        "d290f1ee-6c54-4b01-90E6-d701748fo851",
-	Menuitems: menuitem,
+var testOrderResponse = orderResponse{
+	order: newOrder,
+	OrderedAtTimeStamp: "1613758423",
+	Cost:               999,
 }
+
+var driver = "mysql"
+var dataSourceName = "root:Qwerty123@/task"
 
 func getOrders(w http.ResponseWriter, r *http.Request) {
+	orders := orders{
+		Orders: []order {newOrder,
+		},
+	}
 	b, err := json.Marshal(orders)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -63,8 +76,8 @@ func sendResponse(w http.ResponseWriter, b []byte, err interface{}) {
 func getOrder(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["ID"]
-	if id == order.Id {
-		b, err := json.Marshal(order)
+	if id == newOrder.Id {
+		b, err := json.Marshal(newOrder)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -73,12 +86,14 @@ func getOrder(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+
 func Router() http.Handler {
 	r := mux.NewRouter()
 	s := r.PathPrefix("/api/v1").Subrouter()
 	s.HandleFunc("/orders", getOrders)
 	s.HandleFunc("/hello-world", helloWorld).Methods(http.MethodGet)
 	s.HandleFunc("/order/{ID}", getOrder)
+	//s.HandleFunc("/order", createOrder).Methods(http.MethodPost)
 	return logMiddleware(r)
 }
 
@@ -87,10 +102,10 @@ func logMiddleware(h http.Handler) http.Handler {
 		startTime := time.Now()
 		endTime := time.Now()
 		log.WithFields(log.Fields{
-			"method": r.Method,
-			"url": r.URL,
+			"method":     r.Method,
+			"url":        r.URL,
 			"remoteAddr": r.RemoteAddr,
-			"time": int(endTime.Sub(startTime)),
+			"time":       int(endTime.Sub(startTime)),
 		}).Info("got a new request")
 		h.ServeHTTP(w, r)
 		endTime = time.Now()
